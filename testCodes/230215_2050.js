4540c85056d21da1a615a02c546b9f29f5736cd7
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-16 19:54:11 +0900

    up

--- testCodes/230215_2050.js
+++ testCodes/230215_2050.js
@@ -8,34 +8,20 @@ out vec4 fragColor;
 uniform vec2 u_resolution;
 uniform float u_time;
 
-uint k = 0x456789abu;// 算術積に使う大きな桁数の定数
-const uint UINT_MAX = 0xffffffffu;
-
-uint uhash11(uint n) {
-  n ^= (n << 1);  // 1左シフトして`XOR`
-  n ^= (n >> 1);  // 1右シフトして`XOR`
-  n *= k;         // 算術積
-  n ^= (n << 1);  // 1左シフトして`XOR`
-  return n * k;   // 算術積
-}
-
-
 void main() {
   vec2 pos = gl_FragCoord.xy / u_resolution.xy;
   pos *= vec2(32.0, 9.0);
-  uint u = uhash11(floatBitsToUint(1.2));
-  float fu = float(u);
-  float fm = float(UINT_MAX);
+  float t = 110.9;
   uint[9] a = uint[](
-    floatBitsToUint(1.2),
-    u,
-    floatBitsToUint(fu),
-    0u,
-    UINT_MAX,
-    floatBitsToUint(fm),
-    0u,
-    floatBitsToUint(fu / fm),
-    0u
+    uint(t),
+    uint(-t),
+    floatBitsToUint(t),
+    0xbu ^ 9u,
+    0xffffffffu,
+    0xffffffffu + uint(t),
+    floatBitsToUint(floor(t)),
+    floatBitsToUint(-floor(t)),
+    floatBitsToUint(11.5625)
   );
   
   if (fract(pos.x) < 0.1) {
@@ -56,5 +42,3 @@ void main() {
     fragColor = vec4(vec3(b), 1.0); 
   }
 }
-
-
